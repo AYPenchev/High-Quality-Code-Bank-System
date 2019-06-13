4facc795fa26967f4490f5ce533304a259fe62cd
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-13 05:22:04 -0400

    class Bank
    This class contains only the name of the bank and container of bank accounts that the bank serves

--- HighQualityCode_BankSystem/BankSystemHQC/Bank.cs
+++ HighQualityCode_BankSystem/BankSystemHQC/Bank.cs
@@ -1,6 +1,22 @@
 ﻿namespace BankSystemHQC
 {
-    class Bank
+    using System.Collections.Generic;
+
+    public class Bank
     {
+        public Bank()
+        {
+            this.AccountTypes = new List<BankAccount>();
+            this.Name = string.Empty;
+        }
+
+        public Bank(List<BankAccount> accountTypes, string name)
+        {
+            this.AccountTypes = accountTypes;
+            this.Name = name;
+        }
+
+        public List<BankAccount> AccountTypes { get; set; }
+        public string Name { get; set; }
     }
 }
