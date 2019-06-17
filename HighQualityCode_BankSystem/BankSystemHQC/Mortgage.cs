144336c0111e81deab9eac6a80c27eac7460db2d
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-17 10:14:05 -0400

    Bank development branch
    This class implements Mortgage Bank Account

--- HighQualityCode_BankSystem/BankSystemHQC/Mortgage.cs
+++ HighQualityCode_BankSystem/BankSystemHQC/Mortgage.cs
@@ -1,6 +1,39 @@
 ﻿namespace BankSystemHQC
 {
-    class Mortgage
+    public class Mortgage : BankAccount, IDeposit
     {
+        public const string MORTGAGE = "Mortgage";
+        private const int MONTHS_WITH_HALF_INTEREST = 12;
+        private const double HALF_INTEREST = 0.5;
+
+        public Mortgage() : base()
+        {
+
+        }
+
+        public Mortgage(Customer customer, double interestRate, decimal dueAmount) : base(customer, interestRate)
+        {
+            this.DueAmount = dueAmount;
+        }
+
+        public override string Name => MORTGAGE;
+
+        public override double GetInterestAmount(int numberOfMonths)
+        {
+            double interestAmount = 0;
+
+            if (numberOfMonths >= MONTHS_WITH_HALF_INTEREST)
+            {
+                numberOfMonths -= MONTHS_WITH_HALF_INTEREST;
+                interestAmount = interestAmount + MONTHS_WITH_HALF_INTEREST * HALF_INTEREST * this.InterestRate;
+            }
+            interestAmount = interestAmount + numberOfMonths * this.InterestRate;
+            return interestAmount;
+        }
+
+        public void MakeDeposit(decimal depositAmount)
+        {
+            this.CurrentAmount += depositAmount;
+        }
     }
 }
