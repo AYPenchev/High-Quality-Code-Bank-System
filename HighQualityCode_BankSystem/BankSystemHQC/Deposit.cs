4906a97ceb36f0f5e17f02274a70b06c438a3f25
Asen Penchev <asen.penchev@grafixoft.com>
2019-06-17 10:12:06 -0400

    Bank development branch
    This class implements Deposit Bank Account

--- HighQualityCode_BankSystem/BankSystemHQC/Deposit.cs
+++ HighQualityCode_BankSystem/BankSystemHQC/Deposit.cs
@@ -1,6 +1,50 @@
 ﻿namespace BankSystemHQC
 {
-    class Deposit
+    public class Deposit : BankAccount, IDeposit, IWithdraw
     {
+        public const string DEPOSIT = "Deposit";
+
+        public Deposit() : base()
+        {
+
+        }
+
+        public Deposit(Customer customer, double interestRate, decimal currentAmount = 0) : base(customer, interestRate, currentAmount)
+        {
+
+        }
+
+        public override string Name => DEPOSIT;
+
+        public override double GetInterestAmount(int numberOfMonths)
+        {
+            if (this.Customer.GetBalance() > 0 && this.Customer.GetBalance() < 1000)
+            {
+                return 0;
+            }
+
+            double interestAmount = numberOfMonths * this.InterestRate;
+            return interestAmount;
+        }
+
+        public void MakeDeposit(decimal depositAmount)
+        {
+            this.CurrentAmount += depositAmount;
+
+            if (depositAmount > DEPOSIT_LIMIT_WITHOUT_TAX)
+            {
+                this.CurrentAmount = this.CurrentAmount - (depositAmount * DEPOSIT_PERCENT_TAX);
+            }
+        }
+
+        public void Withdraw(decimal withdrawAmount)
+        {
+            this.CurrentAmount = this.CurrentAmount - (withdrawAmount + this.CalculatePercentageOf(withdrawAmount, WITHDRAW_PERCENT_TAX));
+        }
+
+        private decimal CalculatePercentageOf(decimal amount, decimal percent)
+        {
+            return amount * percent / 100;
+        }
     }
 }
